69f721f1963b45406de9ffd7cf89a090f4181d30
Nguyễn Thị Huyền Trang <nguyentrangg11022004@gmail.com>
2025-12-05 07:20:06 +0700

    update

--- src/segmentation.py
+++ src/segmentation.py
@@ -1,13 +1,3 @@
-# import cv2
-
-# def segment_defects(edge_img):
-#     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
-#     dilated = cv2.dilate(edge_img, kernel, iterations=2)
-
-#     contours, _ = cv2.findContours(dilated, cv2.RETR_EXTERNAL,
-#                                    cv2.CHAIN_APPROX_SIMPLE)
-#     return contours
-    
 import cv2
 import numpy as np
 
